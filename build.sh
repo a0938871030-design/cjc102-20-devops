771d779ce368a165f3ee3adebfc4ca5d75eca90e
a0938871030-design <a0938871030@gamil.com>
2025-12-24 14:32:54 +0800

    ver4.0 first commit

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 set -ex
 #set registry
-USERNAME=129729052534.dkr.ecr.ap-northeast-1.amazonaws.com
+USERNAME=613468529436.dkr.ecr.ap-northeast-1.amazonaws.com
 # image name
-IMAGE=iii-devops-ecr
+IMAGE=cjc102-20-ecr-repo
 docker build -t $USERNAME/$IMAGE:latest .
 version=`cat VERSION`
 echo "version: $version"
